e424cd6f1dac82fff4e78ee0d254009ef31e58ff
Nhat Thanh Tran <binnhathanhtran@gmail.com>
2024-07-23 23:23:22 -0700

    create folder structure and start simple attention, encoder, decoder structure

---
+++ layers/Attention_Family.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class FullAttention(nn.Module):
+    def __init__(self): # it needs more inputs
+        super(FullAttention, self).__init__()
+        #do more stuffs
+
+    def forward(self): # it needs more inputs
+        
+        #do stuffs
+        pass
---
+++ layers/Transformer_EncDec.py
@@ -0,0 +1,10 @@
+import torch
+
+# build classes for enc and dec using classes from attention family 
+# Need encoder and decoder layers classes. Then build encoder and encoder 
+# using these layers, i.e. stack them up. You can define the feedforward here
+# or in the attention family code your choice.
+# In model.Transformer we will use all of the class define here,
+# maybe take a look at it to see how the class in this code use in a higher level.
+# Note the transformer example is for timeseries at the moment.
+
