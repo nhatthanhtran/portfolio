a71a450074903cde12d7ec9e101c8b0ddba3d657
Nhat Thanh Tran <binnhathanhtran@gmail.com>
2024-09-11 22:27:06 -0700

    clean up training

--- layers/Embed.py
+++ layers/Embed.py
@@ -34,7 +34,7 @@ class TokenEmbedding(nn.Module):
             if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(
                     m.weight, mode='fan_in', nonlinearity='leaky_relu')
-
+        
     def forward(self, x):
         x = self.tokenConv(x.permute(0, 2, 1)).transpose(1, 2)
         return x
